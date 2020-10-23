af4fc5e634604cd1a3bcdfa968c40e0a3be9fbae
Adam Miedema <armgitaar@yahoo.com>
2020-10-23 11:50:33 -0600

    no message

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca788a609fc051809be9b6c37790d684'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1fe6d771-61d7-4db8-84b0-8fc604706c7b'
+};
